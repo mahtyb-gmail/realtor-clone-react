f64addcd2fff9f649955777e9718cb149e9d5394
Martin Burke <martin@leburke.com>
2024-08-08 15:43:59 -0700

    Create listing page and use Swiper to add the image slider

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -1,3 +1,67 @@
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import Spinner from "../components/Spinner"
+
+// Swiper
+import { Swiper, SwiperSlide } from "swiper/react"
+import SwiperCore, {
+	EffectFade,
+	Autoplay,
+	Navigation,
+	Pagination,
+} from "swiper"
+import "swiper/css/bundle"
+
+// Firebase Needed Imports
+import { db } from "../firebase"
+import { getAuth } from "firebase/auth"
+import { doc, getDoc } from "firebase/firestore"
+
 export default function Listing() {
-	return <div>Listing</div>
+	const auth = getAuth()
+	const params = useParams()
+	const [listing, setListing] = useState(null)
+	const [loading, setLoading] = useState(true)
+	SwiperCore.use([Autoplay, Navigation, Pagination])
+
+	useEffect(() => {
+		async function fetchListing() {
+			const docRef = doc(db, "listings", params.listingId)
+			const docSnap = await getDoc(docRef)
+			if (docSnap.exists()) {
+				setListing(docSnap.data())
+				setLoading(false)
+			}
+		}
+		fetchListing()
+	}, [params.listingId])
+
+	if (loading) {
+		return <Spinner />
+	}
+
+	return (
+		<main>
+			<Swiper
+				slidesPerView={1}
+				navigation
+				pagination={{ type: "progressbar" }}
+				effect="fade"
+				modules={[EffectFade]}
+				autoplay={{ delay: 3000 }}
+			>
+				{listing.imgUrls.map((url, index) => (
+					<SwiperSlide key={index}>
+						<div
+							className="relative w-full overflow-hidden h-[300px]"
+							style={{
+								background: `url(${listing.imgUrls[index]}) center no-repeat`,
+								backgroundSize: "cover",
+							}}
+						></div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</main>
+	)
 }
