3c3bc133082370494a0eca4463d48187ecf8e159
Martin Burke <martin@leburke.com>
2024-08-07 18:01:15 -0700

    Add delete and edit functionality to the my listing function

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,6 +6,7 @@ import { db } from "../firebase"
 import { FcHome } from "react-icons/fc"
 import {
 	collection,
+	deleteDoc,
 	doc,
 	getDocs,
 	orderBy,
@@ -83,6 +84,20 @@ export default function Profile() {
 		fetchUserListings()
 	}, [auth.currentUser.uid])
 
+	async function onDelete(listingID) {
+		if (window.confirm("Are you sure you want to delete?")) {
+			await deleteDoc(doc(db, "listings", listingID))
+			const updatedListings = listings.filter(
+				(listing) => listing.id !== listingID
+			)
+			setListings(updatedListings)
+			toast.success("Successfully deleted the listing")
+		}
+	}
+	function onEdit(listingID) {
+		navigate(`/edit-listing/${listingID}`)
+	}
+
 	return (
 		<>
 			<section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -158,6 +173,8 @@ export default function Profile() {
 									key={listing.id}
 									id={listing.id}
 									listing={listing.data}
+									onDelete={() => onDelete(listing.id)}
+									onEdit={() => onEdit(listing.id)}
 								/>
 							))}
 						</ul>
